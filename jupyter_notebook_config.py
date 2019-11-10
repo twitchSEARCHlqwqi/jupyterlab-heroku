4be55ed2b93adef0042ab99e48b04af4f4f13596
OliverCWY <chen.wuyang@dhs.sg>
2019-11-10 22:07:41 +0800

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,5 +4,4 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.tokenUnicode=""
-c.NotebookApp.passwordUnicode=""
+c.NotebookApp.passwordUnicode='sha1:47eb64919370:680c1e986e4d7f1a0f4e46444514dd311284a24e'
